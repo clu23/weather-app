5f70d5abb6665564ffd4cc661ef77673bae6457b
clu23 <clement.lulurga@orange.fr>
2024-05-01 22:52:29 +0200

    modifying getData function to handle errors

--- src/index.js
+++ src/index.js
@@ -30,9 +30,13 @@ function home() {
 //This function takes a location as input and fetch the data from weatherAPI, it returns a JSON file 
 async function getData(location){
     location=location.toLowerCase();
-    const response=await fetch(`https://api.weatherapi.com/v1/current.json?key=c7103df777a44f889d2184454240105&q=${location}`);
-    const weatherData= await response.json();
-    console.log(weatherData);
+    try{
+        const response=await fetch(`https://api.weatherapi.com/v1/current.json?key=c7103df777a44f889d2184454240105&q=${location}`);
+        const weatherData= await response.json();
+        console.log(weatherData);
+    } catch(error){
+        alert(error);
+    }   
 }
 
 

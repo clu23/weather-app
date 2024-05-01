b0eb74bdb393779e83a8713a9404843462cd1345
clu23 <clement.lulurga@orange.fr>
2024-05-01 22:38:16 +0200

    adding a function to fetch data

--- src/index.js
+++ src/index.js
@@ -27,6 +27,16 @@ function home() {
 //   renderFooter();
   }
 
+//This function takes a location as input and fetch the data from weatherAPI, it returns a JSON file 
+async function getData(location){
+    location=location.toLowerCase();
+    const response=await fetch(`https://api.weatherapi.com/v1/current.json?key=c7103df777a44f889d2184454240105&q=${location}`);
+    const weatherData= await response.json();
+    console.log(weatherData);
+}
+
+
 home();
+getData('paris');
 
 export { createHtmlElement, $content };
